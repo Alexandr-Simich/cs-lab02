a02fc5549dbb134e78b7130552c761fec135402e
Bob (SimichAB) <simi55@mail.ru>
2020-03-23 02:50:38 +0300

    code: минимум

--- main.cpp
+++ main.cpp
@@ -11,7 +11,15 @@ cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
      << "A / B = " << a / b << '\n';
+
      if (a>b){
+            cout << a;
+     }
+     else{
+        cout << b;
+     }
+     if (a<b){
+
         cout << a;
      }
      else {
